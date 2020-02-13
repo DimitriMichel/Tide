f87b9b5865e6237e62724effc4a414071cb2693a
Dimitri PL <dimitrimachines@gmail.com>
2020-02-13 18:55:26 -0500

    More News Links

--- src/util/NewsImageLinks.js
+++ src/util/NewsImageLinks.js
@@ -1,11 +1,11 @@
 export const imageLinkContainer = {
-    engadget:
+    "engadget":
         "https://pbs.twimg.com/profile_images/655059892022022144/Pq3Q_1oU_400x400.png",
-    Politico:
+    "Politico":
         "https://pbs.twimg.com/profile_images/677177503694237697/y6yTzWn6_400x400.png",
     "Fox News":
         "https://pbs.twimg.com/profile_images/918480715158716419/4X8oCbge_400x400.jpg",
-    CNN:
+    "CNN":
         "https://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_400x400.png",
     "Nytimes.com":
         "https://pbs.twimg.com/profile_images/1098244578472280064/gjkVMelR_400x400.png",
@@ -13,7 +13,7 @@ export const imageLinkContainer = {
         "https://pbs.twimg.com/profile_images/988382060443250689/DijesdNB_400x400.jpg",
     "Deadline.com":
         "https://pbs.twimg.com/profile_images/1116019573981974528/2El9E56p_400x400.png",
-    CNBC:
+   "CNBC":
         "https://pbs.twimg.com/profile_images/1185182366156894208/pKRddT3o_400x400.png",
     "Variety.com":
         "https://pbs.twimg.com/profile_images/1060236422500704257/jltNN1hQ_400x400.jpg",
@@ -50,6 +50,17 @@ export const imageLinkContainer = {
     "The Verge":
         "https://pbs.twimg.com/profile_images/877903823133704194/Mqp1PXU8_400x400.jpg",
     "Foxbusiness.com":
-        "https://pbs.twimg.com/profile_images/875384861736841217/j4vwsisS_400x400.jpg"
+        "https://pbs.twimg.com/profile_images/875384861736841217/j4vwsisS_400x400.jpg",
+    "Theguardian.com":
+        "https://pbs.twimg.com/profile_images/1175141826870861825/K2qKoGla_400x400.png",
+    "Al Jazeera English":
+        "https://pbs.twimg.com/profile_images/1190503555901394944/T4qowO0X_400x400.jpg",
+    "Ars Technica":
+        "https://pbs.twimg.com/profile_images/2215576731/ars-logo_400x400.png",
+    "Livescience.com":
+        "https://pbs.twimg.com/profile_images/760580768758390784/hGfuw8CC_400x400.jpg",
+    "USA Today":
+        "https://pbs.twimg.com/profile_images/924991591642796032/v90LrlR__400x400.jpg",
+
 };
 export default (imageLinkContainer)
